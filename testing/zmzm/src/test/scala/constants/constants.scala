d5007a3b65dfd89df7dd0fc83458de0f10d10948
Yaron <Yaron.esquivel@axity.com>
2024-05-15 22:08:34 -0600

    subiendo archivo

---
+++ testing/zmzm/src/test/scala/constants/constants.scala
@@ -0,0 +1,24 @@
+package Constants
+
+object Constants {
+
+  val BASE_URL_API_FORMS = "/api/form"
+
+  val FORM_BODY_CREATE_JSON = "bodies/formCreate.json"
+  val FORM_BODY_UPDATE_JSON = "bodies/formUpdate.json"
+
+  val FORM_ID_PARAMETER_NAME = "formId"
+
+
+  // HEADERS
+  val HEADER_ACCEPT = "accept"
+  val CONTENT_HEADER ="content-type"
+
+  // header values
+  val TEXT_PLAIN = "text/plain"
+  val APPLICATION_JSON = "application/json"
+
+
+  //URI ENDPOINTS 
+  val URI_USER= "/gatling/user"
+}
